4682515a4b3a933383ae6ae8da4149819b425ccf
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2019-08-27 16:49:37 -0500

    doc dependency

--- sphinx_docs/conf.py
+++ sphinx_docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Resuelve'
 author = 'Miguel Asencio'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
